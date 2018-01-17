d8e2c3ceadcad6cb565633fe1aee8c222f4a55c5
valeb <vabe95@gmail.com>
2018-01-17 19:37:11 +0100

    Add Python and Octave test file, Pythion cheat sheet.

---
+++ Octave.m
@@ -0,0 +1,2 @@
+clear; close all; clc;
+plot(1:100);
---
+++ PythonCheatSheet.py
@@ -0,0 +1,32 @@
+# Baiscs:
+
+# Defining Variables:
+var = 999
+array = [1, 2, 3] 
+array[0] = 999;
+
+
+# "If" statement:
+if var == 999 and array[0] == 999 or var == 999:
+    # indented four spaces
+    print("var is 999")
+
+# While loop
+i = 0
+while i < 10:
+    print(i)
+    i = i + 1
+
+# For loop
+for counter in range(1, 6):
+    print(counter)
+
+# Functions
+
+def functionname( parameters ):
+   "function_docstring"
+   function_suite
+   return [expression]
+
+
+
